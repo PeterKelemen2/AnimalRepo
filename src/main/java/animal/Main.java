b8d777173e8da9e7b64dee339005e12afd3cbf39
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-28 17:53:02 +0100

    Added Animal instance to Main

--- src/main/java/animal/Main.java
+++ src/main/java/animal/Main.java
@@ -2,6 +2,9 @@ package animal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Animal animal = new Animal("Lion", AnimalSize.MEDIUM);
+
+        System.out.println(animal);
     }
 }
