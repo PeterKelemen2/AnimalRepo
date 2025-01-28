e5915281e04fb744a7832f9e1509c45ee71ab46b
Péter Kelemen <peter.kelemen456@gmail.com>
2025-01-28 17:44:19 +0100

    Expanded on Animal

--- src/main/java/animal/Animal.java
+++ src/main/java/animal/Animal.java
@@ -1,5 +1,19 @@
 package animal;
 
 public class Animal {
+    private String name;
+    private AnimalSize size;
 
+    public Animal(String name, AnimalSize size) {
+        this.name = name;
+        this.size = size;
+
+        if (size == AnimalSize.MEDIUM) {
+            System.out.println("Animal is medium size");
+        }
+
+        if (name.equals("newName")) {
+            System.out.println(name);
+        }
+    }
 }
---
+++ src/main/java/animal/AnimalSize.java
@@ -0,0 +1,7 @@
+package animal;
+
+public enum AnimalSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
